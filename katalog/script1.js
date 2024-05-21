0821385846ee10286b47afdbfbfd350158ec9708
Андрей Уханов <97408793+AndreyUkhanov4@users.noreply.github.com>
2024-05-21 13:03:38 +0300

    commit

---
+++ katalog/script1.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".buton111-heder").click(function () {
+    $(".prava").css("display", "flex");
+  });
+});
+
+$(document).ready(function () {
+  $(".buton111-heder").click(function () {
+    $(".pravki").css("display", "block");
+  });
+});
+
+$(document).ready(function () {
+  $(".buton1112-heder").click(function () {
+    $(".prava").css("display", "none");
+  });
+});
+
+$(document).ready(function () {
+  $(".buton1112-heder").click(function () {
+    $(".pravki").css("display", "none");
+  });
+});
---
+++ polozenie/script23.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".buton11-heder").click(function () {
+    $(".buton12-heder").css("display", "block");
+  });
+});
+$(document).ready(function () {
+  $(".buton122-heder").click(function () {
+    $(".buton12-heder").css("display", "none");
+  });
+});
